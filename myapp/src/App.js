f75a5e11d7edf6af387c9dceb60cd0714f95d168
divyam <divyam.prakash@eximietas.com>
2024-01-07 15:55:58 +0530

    app.js changed

---
+++ myapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+    <h2>List with Different Bullets</h2>
+    <ul class="custom-list">
+      <li class="circle">Item 1</li>
+      <li class="square">Item 2</li>
+      <li class="star">Item 4</li>
+      <li class="custom">Item 5</li>
+    </ul>
+  </div>
+  );
+}
+
+export default App;
